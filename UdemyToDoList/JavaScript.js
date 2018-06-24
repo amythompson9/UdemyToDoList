eceb8df5563320e9727cda87179e9ff683b4eb9b
Amy Thompson <amythompson9@gmail.com>
2018-06-23 21:33:59 -0400

    To Do List

--- UdemyToDoList/JavaScript.js
+++ UdemyToDoList/JavaScript.js
@@ -1,3 +1,18 @@
-﻿window.setTimeout(funtion() {
+﻿    var todos = [];
+    var input = prompt("What would you like to do?");
 
-}, 500);
+    while(input !== "quit") {
+        //handle input
+        if (input === "list") {
+            console.log(todos);
+        }
+        else if (input === "new") {
+            //ask for new todo
+            var newTodo = prompt("Enter new To Do");
+            //add to todos array
+            todos.push(newToDo);
+        }
+        //ask again for new input
+        var input = prompt("What would you like to do?");
+    }
+    console.log("Ok. You quit the app.");
